58df12f44825debdf3119556171b2db0e5212ad8
Sasha Havrylov <sashagavrilov1995@gmail.com>
2022-07-18 10:07:52 +0300

    offsendgrid

--- bin/server.js
+++ bin/server.js
@@ -1,24 +1,17 @@
-const app = require('../app')
-const mongoose = require("mongoose")
-const {DB_HOST, PORT = 3000} = process.env
+const app = require("../app");
+const mongoose = require("mongoose");
+const { DB_HOST, PORT = 3000 } = process.env;
+console.log(DB_HOST, "DB HOST");
 
-
-mongoose.connect(DB_HOST)
-  .then(()=> {
-
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`)
-    })
-    console.log("Database connection successful")
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+    console.log("Database connection successful");
   })
-  .catch(error => {
-  console.log(error.message)
-  process.exit(1)
-}
-  );
-
-
-
-
-
-
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
--- models/user.js
+++ models/user.js
@@ -1,58 +1,57 @@
 const { Schema, model } = require("mongoose");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
-const userSchema = Schema({
-  password: {
-    type: String,
-    required: [true, "Password is required"],
+const userSchema = Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    avatarUrl: {
+      type: String,
+    },
+    verify: {
+      type: Boolean,
+      default: true,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter",
-  },
-  token: {
-    type: String,
-    default: null,
-  },
-  avatarUrl: {
-    type: String,
-  },
-  verify: {
-    type: Boolean,
-    default: false,
-  },
-  verificationToken: {
-    type: String,
-    required: [true, 'Verify token is required'],
-  },
-}, {versionKey: false, timestamps: true});
+  { versionKey: false, timestamps: true }
+);
 
-
-userSchema.methods.comparePassword = function(password){
+userSchema.methods.comparePassword = function (password) {
   return bcrypt.compareSync(password, this.password);
-}
-
+};
 
 const Joi = require("joi");
 
-
 const joiSchema = Joi.object({
-    password: Joi.string().min(5).max(35).required(),
-    email: Joi.string().required(),
-    subscription: Joi.string(),
-    token: Joi.string()
-    })
+  password: Joi.string().min(5).max(35).required(),
+  email: Joi.string().required(),
+  subscription: Joi.string(),
+  token: Joi.string(),
+});
 
+const User = model("user", userSchema);
 
-    const User = model("user", userSchema)
-
-    module.exports = {
-        User,
-        joiSchema
-    }
+module.exports = {
+  User,
+  joiSchema,
+};
